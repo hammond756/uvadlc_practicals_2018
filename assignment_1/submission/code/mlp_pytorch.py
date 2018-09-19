c3fab55c9666ecb887fac848e0d4c7e916d1a3a7
aronhammond <hammond756@live.nl>
2018-09-19 21:57:02 +0200

    stuff

---
+++ assignment_1/submission/code/mlp_pytorch.py
@@ -0,0 +1,71 @@
+"""
+This module implements a multi-layer perceptron (MLP) in PyTorch.
+You should fill in code into indicated sections.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from torch import nn, functional
+from functools import reduce
+
+class MLP(nn.Module):
+  """
+  This class implements a Multi-layer Perceptron in PyTorch.
+  It handles the different layers and parameters of the model.
+  Once initialized an MLP object can perform forward.
+  """
+
+  def __init__(self, n_inputs, n_hidden, n_classes):
+    network__ = """
+    Initializes MLP object.
+
+    Args:
+      n_inputs: number of inputs.
+      n_hidden: list of ints, specifies the number of units
+                in each linear layer. If the list is empty, the MLP
+                will not have any linear layers, and the model
+                will simply perform a multinomial logistic regression.
+      n_classes: number of classes of the classification problem.
+                 This number is required in order to specify the
+                 output dimensions of the MLP
+
+    TODO:
+    Implement initialization of the network.
+    """
+
+    super(MLP, self).__init__()
+
+    if len(n_hidden) == 0:
+      modules = [nn.Linear(n_inputs, n_classes)]
+    else:
+      modules = [nn.Linear(n_inputs, n_hidden[0])]
+      modules.append(nn.ReLU())
+
+      for i in range(1, len(n_hidden)):
+        modules.append(nn.Linear(n_hidden[i-1], n_hidden[i]))
+        modules.append(nn.ReLU())
+
+      modules.append(nn.Linear(n_hidden[-1], n_classes))
+
+    self.net = nn.ModuleList(modules)
+
+  def forward(self, x):
+    """
+    Performs forward pass of the input. Here an input tensor x is transformed through 
+    several layer transformations.
+    
+    Args:
+      x: input to the network
+    Returns:
+      out: outputs of the network
+    
+    TODO:
+    Implement forward pass of the network.
+    """
+    out = reduce(lambda output, module: module(output), self.net, x)
+
+    return out
+
+
+model = MLP(10, [15, 40, 3], 2)
---
+++ assignment_1/submission/code/modules.py
@@ -0,0 +1,228 @@
+"""
+This module implements various modules of the network.
+You should fill in code into indicated sections.
+"""
+import numpy as np
+
+def exp_normalize_batch(x):
+    b = x.max(axis=1)[:, None]
+    y = np.exp(x - b)
+    return y / y.sum(axis=1)[:, None]
+
+class LinearModule(object):
+
+  """
+  Linear module. Applies a linear transformation to the input data. 
+  """
+  def __init__(self, in_features, out_features):
+    """
+    Initializes the parameters of the module. 
+    
+    Args:
+      in_features: size of each input sample
+      out_features: size of each output sample
+
+    TODO:
+    Initialize weights self.params['weight'] using normal distribution with mean = 0 and 
+    std = 0.0001. Initialize biases self.params['bias'] with 0. 
+    
+    Also, initialize gradients with zeros.
+    """
+
+    self.params = {
+        'weight': np.random.normal(loc=0, scale=0.0001, size=(in_features, out_features)),
+        'bias': np.zeros(out_features)
+    }
+
+    self.grads = {
+        'weight': np.zeros_like(self.params['weight']),
+        'bias': np.zeros_like(self.params['bias'])
+    }
+
+  def forward(self, x):
+    """
+    Forward pass.
+    
+    Args:
+      x: input to the module
+    Returns:
+      out: output of the module
+    
+    TODO:
+    Implement forward pass of the module. 
+    
+    Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.                                                           #
+    """
+
+
+    self.prev_x = x
+
+    # Expand dimensions of parameters so we can matmul with batched input
+    W = self.params['weight'][None, :]
+    b = self.params['bias'][None, :]
+
+    out = np.matmul(x, W) + b
+
+    # TODO: is this needed?
+    # remove extra dimension
+    out = out.squeeze(0)
+
+    return out
+
+  def backward(self, dout):
+    """
+    Backward pass.
+
+    Args:
+      dout: gradients of the previous module
+    Returns:
+      dx: gradients with respect to the input of the module
+    
+    TODO:
+    Implement backward pass of the module. Store gradient of the loss with respect to 
+    layer parameters in self.grads['weight'] and self.grads['bias']. 
+    """
+
+     # TODO: Check this: this way of averaging feels kinda implicit
+    self.grads['weight'] = np.matmul(self.prev_x.T, dout)
+    self.grads['bias'] = dout.mean(axis=0)
+
+    assert self.grads['weight'].shape == self.params['weight'].shape, "Gradient matrix should be the same shape as params: {}, {}".format(self.grads['weight'].shape, self.params['weight'].shape)
+
+    dx = np.matmul(dout, self.params['weight'].T)
+    
+    return dx
+
+class ReLUModule(object):
+  """
+  ReLU activation module.
+  """
+  def forward(self, x):
+    """
+    Forward pass.
+    
+    Args:
+      x: input to the module
+    Returns:
+      out: output of the module
+    
+    TODO:
+    Implement forward pass of the module. 
+    
+    Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.                                                           #
+    """
+
+    self.prev_x = x
+    out = x*(x > 0.0)
+
+    return out
+
+  def backward(self, dout):
+    """
+    Backward pass.
+
+    Args:
+      dout: gradients of the previous module
+    Returns:
+      dx: gradients with respect to the input of the module
+    
+    TODO:
+    Implement backward pass of the module.
+    """
+
+    dx = dout*(self.prev_x > 0)
+
+    return dx
+
+class SoftMaxModule(object):
+  """
+  Softmax activation module.
+  """
+  def forward(self, x):
+    """
+    Forward pass.
+    Args:
+      x: input to the module
+    Returns:
+      out: output of the module
+    
+    TODO:
+    Implement forward pass of the module. 
+    To stabilize computation you should use the so-called Max Trick - https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
+    
+    Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.                                                           #
+    """
+
+    self.S = exp_normalize_batch(x)
+    out = self.S
+
+    return out
+
+  def backward(self, dout):
+    """
+    Backward pass.
+
+    Args:
+      dout: gradients of the previous module
+    Returns:
+      dx: gradients with respect to the input of the module
+    
+    TODO:
+    Implement backward pass of the module.
+    """
+
+
+    # calculate components of jacobians
+    dsoft = -np.einsum('ij,ik->ijk', self.S, self.S) # NxD * NxD -> NxDxD
+    diag = np.multiply(self.S, (1 - self.S)) # -> NxD (diagonals)
+
+    # replace N diagonals wis s(1-s) for i=j
+    diag_idx = np.arange(dout.shape[1])
+    dsoft[:, diag_idx, diag_idx] = diag
+
+    dx = np.einsum('ik,ijk->ij', dout, dsoft)
+
+    return dx
+
+class CrossEntropyModule(object):
+  """
+  Cross entropy loss module.
+  """
+  def forward(self, x, y):
+    """
+    Forward pass.
+
+    Args:
+      x: input to the module
+      y: labels of the input
+    Returns:
+      out: cross entropy loss
+    
+    TODO:
+    Implement forward pass of the module. 
+    """
+
+    idxs = np.argmax(y, axis=1)
+    out = -np.log(x[range(x.shape[0]), idxs])
+    out = out.mean()
+
+    return out
+
+  def backward(self, x, y):
+    """
+    Backward pass.
+
+    Args:
+      x: input to the module
+      y: labels of the input
+    Returns:
+      dx: gradient of the loss with the respect to the input x.
+    
+    TODO:
+    Implement backward pass of the module.
+    """
+
+    dx = -y / (x + 1e-6)
+    dx /= x.shape[0]
+
+    return dx
