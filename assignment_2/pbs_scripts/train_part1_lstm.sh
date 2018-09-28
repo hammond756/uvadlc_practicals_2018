359d6b161bdfa0b43d7e20fa20e1f5286425a4d1
Aron Hammond <lgpu0034@test3.irc.sara.nl.irc.sara.nl>
2018-09-28 09:44:34 +0200

    corrected working dir in pbs scripts

--- assignment_2/pbs_scripts/train_part1_lstm.sh
+++ assignment_2/pbs_scripts/train_part1_lstm.sh
@@ -6,6 +6,6 @@
 module load Python/3.6.3-foss-2017b cuDNN/7.0.5-CUDA-9.0.176 OpenMPI/2.1.1-GCC-6.4.0-2.28 NCCL
 export LD_LIBRARY_PATH=/hpc/sw/NCCL/2.0.5/lib:/hpc/eb/Debian9/cuDNN/7.0.5-CUDA-9.0.176/lib64:/hpc/eb/Debian9/CUDA/9.0.176/lib64:$LD_LIBRARY_PATH
 
-pip3 install -r ~/uva_deeplearning/requirements.txt --user --no-cache
-
-python ~/uva_deeplearning/assignment_2/part1/train.py --model_type LSTM
+pip3 install -r ~/uvadlc_practicals_2018/requirements.txt --user --no-cache
+cd ~/uvadlc_practicals_2018/assignment_2/part1/
+python train.py --model_type LSTM
--- assignment_2/pbs_scripts/train_part3.sh
+++ assignment_2/pbs_scripts/train_part3.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#PBS -lwalltime=20:00
+#PBS -lwalltime=10:00
 #PBS -lnodes=1:ppn=12
 #PBS -lmem=250GB
 
 module load Python/3.6.3-foss-2017b cuDNN/7.0.5-CUDA-9.0.176 OpenMPI/2.1.1-GCC-6.4.0-2.28 NCCL
 export LD_LIBRARY_PATH=/hpc/sw/NCCL/2.0.5/lib:/hpc/eb/Debian9/cuDNN/7.0.5-CUDA-9.0.176/lib64:/hpc/eb/Debian9/CUDA/9.0.176/lib64:$LD_LIBRARY_PATH
 
-pip3 install -r ~/uva_deeplearning/requirements.txt --user --no-cache
-
-python ~/uva_deeplearning/assignment_2/part3/train.py --txt_file ~/uva_deeplearning/assignment_2/assets/book_NL_darwin_reis_om_de_wereld.txt
+pip3 install -r ~/uvadlc_practicals_2018/requirements.txt --user --no-cache
+cd ~/uvadlc_practicals_2018/assignment_2/part3
+python train.py --txt_file assets/book_NL_darwin_reis_om_de_wereld.txt --train_steps 10000
