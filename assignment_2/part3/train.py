974a1da0c36d443b7a48ece86b8ab822afc4374d
aronhammond <hammond756@live.nl>
2018-09-29 18:34:46 +0200

    save model every epoch

--- assignment_2/part3/train.py
+++ assignment_2/part3/train.py
@@ -72,7 +72,7 @@ def sample_model(model, vocab_size, device=torch.device('cpu'), temp=1.0, hidden
         # convert to one-hot
         prev_char = one_hot(prev_char, vocab_size).to(device)
 
-    output, (h,c) = model(prev_char, hidden_states)
+    output, hidden_states = model(prev_char, hidden_states)
     output = output[:, -1, :] # last prediction
 
     # Sample next character from softmax
@@ -80,7 +80,7 @@ def sample_model(model, vocab_size, device=torch.device('cpu'), temp=1.0, hidden
     next_char = one_hot(next_char, vocab_size)           # [B,1,D]
 
     # concat the recursive predictions to currect character
-    future = sample_model(model, vocab_size, temp=temp, hidden_states=(h,c), n=n-1, prev_char=next_char)
+    future = sample_model(model, vocab_size, temp=temp, hidden_states=hidden_states, n=n-1, prev_char=next_char)
     encoded_text = torch.cat([next_char, future], dim=1)
 
     return encoded_text
@@ -125,79 +125,74 @@ def train(config):
 
     # TODO: configure learning rate scheduler
 
-    for step, (batch_inputs, batch_targets) in enumerate(data_loader):
+    for epoch in range(config.epochs):
+        for step, (batch_inputs, batch_targets) in enumerate(data_loader):
 
-        # Only for time measurement of step through network
-        t1 = time.time()
+            # Only for time measurement of step through network
+            t1 = time.time()
 
-        X = torch.stack(batch_inputs, dim=1)
-        X = one_hot(X, dataset.vocab_size)
-        Y = torch.stack(batch_targets, dim=1)
-        X, Y = X.to(device), Y.to(device)
+            X = torch.stack(batch_inputs, dim=1)
+            X = one_hot(X, dataset.vocab_size)
+            Y = torch.stack(batch_targets, dim=1)
+            X, Y = X.to(device), Y.to(device)
 
-        # forward pass
-        outputs, _ = model(X)
+            # forward pass
+            outputs, _ = model(X)
 
-        # compute training metrics
-        loss = criterion(outputs.transpose(2, 1), Y)
-        predictions = get_predictions(outputs)
-        accuracy = (Y == predictions).sum().item() / reduce(lambda x,y: x*y, Y.size())
+            # compute training metrics
+            loss = criterion(outputs.transpose(2, 1), Y)
+            predictions = get_predictions(outputs)
+            accuracy = (Y == predictions).sum().item() / reduce(lambda x,y: x*y, Y.size())
 
-        # backward pass
-        model.zero_grad()
-        loss.backward()
-        optimizer.step()
+            # backward pass
+            model.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        # clip gradients to prevent them form exploding
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=config.max_norm)
+            # clip gradients to prevent them form exploding
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=config.max_norm)
 
-        # Just for time measurement
-        t2 = time.time()
-        examples_per_second = config.batch_size/float(t2-t1)
+            # Just for time measurement
+            t2 = time.time()
+            examples_per_second = config.batch_size/float(t2-t1)
 
-        if step % config.print_every == 0:
+            if step % config.print_every == 0:
 
-            print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, Examples/Sec = {:.2f}, "
-                  "Accuracy = {:.2f}, Loss = {:.3f}".format(
-                    datetime.now().strftime("%Y-%m-%d %H:%M"), step,
-                    config.train_steps, config.batch_size, examples_per_second,
-                    accuracy, loss
-            ))
+                print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, Examples/Sec = {:.2f}, "
+                      "Accuracy = {:.2f}, Loss = {:.3f}".format(
+                        datetime.now().strftime("%Y-%m-%d %H:%M"), step,
+                        config.train_steps, config.batch_size, examples_per_second,
+                        accuracy, loss
+                ))
 
-            xp.add_scalar_dict({
-                'accuracy' : accuracy,
-                'loss' : loss.item()
-            }, step=step, wall_time=time.time())
+                xp.add_scalar_dict({
+                    'accuracy' : accuracy,
+                    'loss' : loss.item()
+                }, step=step, wall_time=time.time())
 
-        if step % config.sample_every == 0:
-            # Generate some sentences by sampling from the model
-            path = os.path.splitext(config.txt_file)[0] + "_generated_samples_" + "temp_" + str(config.temperature) + ".txt"
-            abs_path = os.path.abspath(path)
-            mode = 'a' if step > 0 else 'w'
-            print("---")
-            print("Write sample to ", path)
-            print("---")
+            if step % config.sample_every == 0:
+                # Generate some sentences by sampling from the model
+                path = os.path.splitext(config.txt_file)[0] + "_generated_samples_" + "temp_" + str(config.temperature) + ".txt"
+                abs_path = os.path.abspath(path)
+                mode = 'a' if step > 0 else 'w'
+                print("---")
+                print("Write sample to ", path)
+                print("---")
 
-            with open(abs_path, mode) as f:
-                progress = "[{}] Train Step {:04d}/{:04d}, Batch Size = {}," \
-                       "Accuracy = {:.2f}, Loss = {:.3f}".format(
-                    datetime.now().strftime("%Y-%m-%d %H:%M"), step,
-                    config.train_steps, config.batch_size, accuracy, loss)
+                with open(abs_path, mode) as f:
+                    progress = "[{}] Train Step {:04d}/{:04d}, Batch Size = {}," \
+                           "Accuracy = {:.2f}, Loss = {:.3f}".format(
+                        datetime.now().strftime("%Y-%m-%d %H:%M"), step,
+                        config.train_steps, config.batch_size, accuracy, loss)
 
-                generated_sequence = sample_model(model, dataset.vocab_size, device=device, temp=config.temperature, n=config.generate_n)
-                sample = string_from_one_hot(generated_sequence, dataset)
+                    generated_sequence = sample_model(model, dataset.vocab_size, device=device, temp=config.temperature, n=config.generate_n)
+                    sample = string_from_one_hot(generated_sequence, dataset)
 
-                f.write(progress + '\n\n')
-                f.write(sample)
-                f.write('\n\n--------\n\n\n')
+                    f.write(progress + '\n\n')
+                    f.write(sample)
+                    f.write('\n\n--------\n\n\n')
 
-        try:
-            if step == config.train_steps:
-                # If you receive a PyTorch data-loader error, check this bug report:
-                # https://github.com/pytorch/pytorch/pull/9655
-                break
-        except:
-            pass
+            torch.save(model, 'grimm_epoch_{}.pt'.format(epoch + 1))
 
     _ = xp.to_zip(experiment_label + ".zip")
     print('Done training.')
@@ -227,6 +222,7 @@ if __name__ == "__main__":
     parser.add_argument('--dropout_keep_prob', type=float, default=1.0, help='Dropout keep probability')
 
     parser.add_argument('--train_steps', type=int, default=int(1e6), help='Number of training steps')
+    parser.add_argument('--epochs', type=int, default=int(10), help='Number of training steps')
     parser.add_argument('--max_norm', type=float, default=5.0, help='--')
 
     # Misc params
