854c0942a3d2a02e58c5800f68f5e4e7af8a3a3c
Aron Hammond <lgpu0034@test3.irc.sara.nl.irc.sara.nl>
2018-09-29 15:22:32 +0200

    moved lstm/rnn runs to one bash file because CrayonServer can't handle parallel training

--- assignment_2/pbs_scripts/train_part1.sh
+++ assignment_2/pbs_scripts/train_part1.sh
@@ -8,7 +8,11 @@ export LD_LIBRARY_PATH=/hpc/sw/NCCL/2.0.5/lib:/hpc/eb/Debian9/cuDNN/7.0.5-CUDA-9
 
 pip3 install -r ~/uvadlc_practicals_2018/requirements.txt --user --no-cache
 cd ~/uvadlc_practicals_2018/assignment_2/part1/
-for T in 5 10 15 20 25
+for T in 20 
 do
 	python train.py --model_type RNN --input_length $T --input_dim 10
+done
+for T in 20
+do
+	python train.py --model_type LSTM --input_length $T --input_dim 10
 done
